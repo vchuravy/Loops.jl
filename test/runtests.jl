77390035ad85879c2cde996d4497133fc012b484
Valentin Churavy <v.churavy@gmail.com>
2023-11-01 14:23:52 -0400

    Process multiple invariant statements

--- test/runtests.jl
+++ test/runtests.jl
@@ -58,3 +58,40 @@ end
     @test callee.name == :sin
     @test ir.stmts[stmts[2]][:inst] isa Core.Compiler.GotoNode
 end
+
+# Move sin(x) to pre-header
+function single_loop_with_multiple_invariant(A, x)
+    acc = zero(eltype(A))
+    for a in A
+        y = sin(3*x)
+        z = 2*y
+        acc += z * a
+    end
+    return acc
+end
+
+@testset "Single-loop w multiple invariant" begin
+    (ir0, rt) = only(Base.code_ircode(single_loop_with_multiple_invariant, (Vector{Float64}, Float64),
+                                     optimize_until = "compact 1"))
+
+    domtree = Core.Compiler.construct_domtree(ir0.cfg.blocks)
+    loops = Loops.construct_loopinfo(ir0, domtree)
+
+    (h, LI) = only(loops)
+    @test h == LI.header
+    @test h ∈ LI.blocks
+    @test all(BB->BB ∈ LI.blocks, LI.latches)
+
+    ir = Core.Compiler.copy(ir0)
+    ir = Loops.licm_pass!(ir)
+
+    # Old header is pre-header
+    stmts = ir.cfg.blocks[h].stmts
+    @test length(stmts) == 4
+    call = ir.stmts[stmts[2]][:inst]
+    @test call isa Expr
+    @test call.head == :call
+    callee = call.args[1]
+    @test callee.name == :sin
+    @test ir.stmts[stmts[4]][:inst] isa Core.Compiler.GotoNode
+end
