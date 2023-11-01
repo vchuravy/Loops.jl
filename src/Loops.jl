9329e2e2b399a1e585b1678a90c9790043a5e52b
Valentin Churavy <v.churavy@gmail.com>
2023-11-01 12:47:04 -0400

    Add licm_pass!

--- src/Loops.jl
+++ src/Loops.jl
@@ -11,13 +11,14 @@ import ._TOP_MOD:     # Base definitions
     Vector, BitSet, IdDict, IdSet, UnitRange, Csize_t, Callable, ∪, ⊆, ∩, :, ∈, ∉, =>,
     in, length, get, first, last, haskey, keys, get!, isempty, isassigned,
     pop!, push!, pushfirst!, empty!, delete!, max, min, enumerate, unwrap_unionall,
-    ismutabletype, collect, iterate, <=, >=, ones, pairs, resize!, copy!, @show, fill!,
-    sort!, Iterators, isless, findfirst
+    ismutabletype, collect, iterate, <=, >=, ones, pairs, 
+    resize!, copy!, @show, fill!,
+    sort!, Iterators, isless, findfirst, filter, map!, map
 
 import Core.Compiler: # Core.Compiler specific definitions
     IRCode, construct_domtree, dominates, block_for_inst, StmtRange, BasicBlock, CFG, ssamap,
     PhiNode, GotoNode, GotoIfNot, isexpr, SSAValue, LineInfoNode, NoCallInfo, cfg_insert_edge!,
-    NewNodeInfo
+    NewNodeInfo, cfg_delete_edge!, IR_FLAG_EFFECT_FREE, Argument, userefs, InsertBefore, NewInstruction
 
 include(x) = _TOP_MOD.include(@__MODULE__, x)
 
@@ -86,6 +87,129 @@ function construct_loopinfo(ir::IRCode, domtree=construct_domtree(ir.cfg.blocks)
 
     # TODO: Loop nesting/Control tree
     return loops 
+end
+
+function invariant(ir, LI, invariant_stmts, stmt)
+    if stmt isa Argument || stmt isa GlobalRef || stmt isa QuoteNode || stmt isa Bool
+        return true
+    elseif stmt isa SSAValue
+        id = stmt.id
+        bb = block_for_inst(ir.cfg, id)
+        if bb ∉ LI.blocks
+            return true
+        end
+        return id ∈ invariant_stmts 
+    end
+    return false
+end
+
+function invariant_expr(ir, LI, invariant_stmts, stmt)
+    invar = true
+    # for useref in userefs(stmt)
+    #     invar &= invariant(ir, LI, invariant_stmts, useref[])
+    # end
+    state = Core.Compiler.iterate(Core.Compiler.userefs(stmt))
+	while state !== nothing
+		useref, next  = state
+
+		invar &= invariant(ir, LI, invariant_stmts, Core.Compiler.getindex(useref))
+		
+		state = Core.Compiler.iterate(Core.Compiler.userefs(stmt), next)
+	end
+    return invar
+end
+
+function invariant_stmt(ir, loop, invariant_stmts, stmt)
+    if stmt isa Expr
+        return invariant_expr(ir, loop, invariant_stmts, stmt)
+    end
+    return invariant(ir, loop, invariant_stmts, stmt)
+end
+
+function find_invariant_stmts(ir, LI)
+	stmts = Int[]
+	for BB in LI.blocks
+		for stmt in ir.cfg.blocks[BB].stmts
+			# Okay to throw
+			if (ir.stmts[stmt][:flag] & IR_FLAG_EFFECT_FREE) != 0
+				# Check if stmt is invariant
+				if invariant_stmt(ir, LI, stmts, ir.stmts[stmt][:inst])
+					push!(stmts, stmt)
+				end
+			end
+		end
+	end
+	return stmts
+end
+
+function insert_preheader!(ir, LI)
+	header = LI.header
+	preds = ir.cfg.blocks[header].preds
+	entering = filter(BB->BB ∉ LI.blocks, preds)
+
+	# split the header
+	split = first(ir.cfg.blocks[header].stmts)
+	info = allocate_goto_sequence!(ir, [split => 0])
+
+	map!(BB->info.bbchangemap[BB], entering, entering)
+
+	preheader = header
+	header = info.bbchangemap[header]
+
+	on_phi_label(i) = i ∈ entering ? preheader : i
+
+	for stmt in ir.cfg.blocks[header].stmts
+		inst = ir.stmts[stmt][:inst]
+		if inst isa PhiNode
+            edges = inst.edges::Vector{Int32}
+            for i in 1:length(edges)
+                edges[i] = on_phi_label(edges[i])
+            end
+		else
+			continue
+		end
+	end
+
+	# TODO: should we mutate LI instead?
+	blocks = map(BB->info.bbchangemap[BB], LI.blocks)
+	latches = map(BB->info.bbchangemap[BB], LI.latches)
+
+	for latch in latches
+		cfg_delete_edge!(ir.cfg, latch, preheader)
+		cfg_insert_edge!(ir.cfg, latch, header)
+		stmt = ir.stmts[last(ir.cfg.blocks[4].stmts)]
+		# stmt[:inst] = GotoNode(header)
+        Compiler.setindex!(stmt, Compiler.GotoNode(header), :inst)
+	end
+
+	Compiler.verify_ir(ir)
+
+	return preheader, LoopInfo(header, latches, blocks)
+end
+
+function move_invariant!(ir, preheader, LI)
+	insertion_point = last(ir.cfg.blocks[preheader].stmts)
+	stmts = find_invariant_stmts(ir, LI)
+	inserter = InsertBefore(ir, SSAValue(insertion_point))
+	for stmt in stmts
+		new_stmt = inserter(NewInstruction(ir.stmts[stmt]))
+		# ir.stmts[stmt] = new_stmt
+        Core.Compiler.setindex!(
+			ir.stmts, new_stmt, stmt)
+	end
+
+end
+
+function licm_pass!(ir::IRCode, loops=construct_loopinfo(ir, construct_domtree(ir.cfg.blocks)))
+    # TODO: Check for benefit
+    # TODO: More than one Loop in the function. `LI` become stale and would need to be recalculated
+    # TODO: Processing order, we should proceess innermost to outermost loops,
+    for (h, LI) in loops
+        # Find stmts that are invariant w.r.t this loop
+        preheader, LI = insert_preheader!(ir, LI)
+        move_invariant!(ir, preheader, LI)
+    end
+    ir = Compiler.compact!(ir)
 end
 
 # TODO: 
